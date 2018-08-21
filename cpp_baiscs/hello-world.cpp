6ddc8c49e0332812a6cd752fcc5261e822a02ede
WENLONG JIANG <jiangwe@us.ibm.com>
2018-08-21 14:03:36 -0400

    main function

---
+++ cpp_baiscs/hello-world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // single line comment
+    /*
+        this 
+        is 
+        a 
+        multiple
+        line
+        comment
+    */
+    cout << "Wenlong is awesome"
+         << endl
+         << "another line";
+
+    return 0;
+}
