6536c71a39e666b3aaee0c5be4e959f7e23cac94
WENLONG JIANG <jiangwe@us.ibm.com>
2018-09-01 11:52:02 -0400

    type casting

--- cpp_baiscs/typecasting.cpp
+++ cpp_baiscs/typecasting.cpp
@@ -4,7 +4,18 @@ using namespace std;
 int main()
 {
     char c = 'a';
-    cout << c + 3;
+    cout << c + 3 << endl;
+
+    char ch = 'A';
+
+    ch = ch + 1;
+    cout << ch << endl;
+
+    int val = (int)ch;
+    cout << val << endl;
+
+    // Implicit bool -> char -> int -> float
+    // explicit float -> int -> char -> bool
 
     return 0;
 }
